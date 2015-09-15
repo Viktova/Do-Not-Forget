21d5882f08b5b0b26abc01cb0bb2a30fbbb79e29
Alexandre Plennevaux <alexandre@pixeline.be>
2015-09-16 01:30:58 +0200

    Cleanup deprecated commented code

--- index.php
+++ index.php
@@ -76,18 +76,26 @@ $f3->route('POST /synchronise-memo',
 		exit;
 });
 
-$f3->route('POST /fetch-memo', function($f3){
-	global $db;
-	$username = $f3->get('POST.email');
-	$id = $f3->get('POST.id');
-	$user = new DB\SQL\Mapper($db, 'memos');
-	$user->load(array('email = :username AND id= :id LIMIT 0,1', ':username'=>$username, ':id'=> $id ));
-	if($user->dry()){
-		die("non existing user.");
-	}
-	$f3->set('SESSION.memo',  stripslashes($user->memo));
-	echo stripslashes($user->memo);
-	exit;
+$f3->route('POST /scrape-url',function($f3){
+	$url = $f3->get('POST.url');
+	require 'classes/SimpleScraper.class.php';
+	$url = isset($url) ? $url : '';
+        try {
+            $scraper = new SimpleScraper($url);
+            $data = $scraper->getAllData();
+            $response = array(
+                'success' => true,
+                'ogp' => $data['ogp'],
+                'dump' => print_r($data, true)
+            );
+        } catch (Exception $e) {
+            $response = array(
+                'success' => false,
+                'message' => 'Something went wrong.',
+                'log' => "$e"
+            );
+        }
+        echo json_encode($response);
 });
 
 $f3->route('GET /tips',
--- ui/js/_functions.jquery.js
+++ ui/js/_functions.jquery.js
@@ -109,41 +109,32 @@ var user = {
 			}
 		}, 1000);
 	}
+};
+
+
+function scrape_url(){
+	// see http://code.ramonkayo.com/simple-scraper/
+	
+	$.post('/scrape-url', {'url' : $('#url').val()},
+		function(json) {
+			if (json.success) {
+				$('#title').text(json.ogp.title);
+				$('#title').attr('href', json.ogp.url);
+				$('#description').text(json.ogp.description);
+				$('#image').attr('src', json.ogp.image);
+				$('#dump').text(json.dump);
+			} else {
+                console.log('SCraping did not work!');
+                console.log(json.log);
+           	}
+        },
+        'json'
+        );
 }
 /*********************************************************
 	 HELPER FUNCTIONS 
 *********************************************************/
-/*
-function reset() {
-	localStorage[localstorage_var_name] = medium.value('hello');
-}
-// useful to remove A anchors from html string
-// http://stackoverflow.com/questions/4536329/whats-the-best-way-to-strip-out-only-the-anchor-html-tags-in-javascript-given
 
-function unwrapAnchors() {
-	if (!('tagName' in this) || this.tagName.toLowerCase() !== 'a' || !('parentNode' in this)) {
-		return;
-	}
-	var childNodes = this.childNodes || [],
-		children = [],
-		child;
-	// Convert childNodes collection to array
-	for (var i = 0, childNodes = this.childNodes || []; i < childNodes.length; i++) {
-		children[i] = childNodes[i];
-	}
-	// Move children outside element
-	for (i = 0; i < children.length; i++) {
-		child = children[i];
-		if (('tagName' in child) && child.tagName.toLowerCase() === 'a') {
-			child.parentNode.removeChild(child);
-		} else {
-			this.parentNode.insertBefore(child, this);
-		}
-	}
-	// Remove now-empty anchor
-	this.parentNode.removeChild(this);
-}
-*/
 // Remove weird Facebook callback token in url #_=_
 
 function remove_facebook_token_in_url() {
@@ -161,12 +152,3 @@ window.console = window.console || (function() {
 	console.log = console.warn = console.debug = console.info = console.error = console.time = console.dir = console.profile = console.clear = console.exception = console.trace = console.assert = function(s) {};
 	return console;
 })();
-// Html entity decoding
-/*
-
-function decodeHtml(html) {
-	var txt = document.createElement("textarea");
-	txt.innerHTML = html;
-	return txt.value;
-}
-*/
